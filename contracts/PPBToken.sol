8b925edf5a395bb87cd765c1e4b5192d4f7c63c1
Zeratosthenes <zeratosthenes@protonmail.com>
2020-08-06 20:00:22 +0800

    Reduce total supply

--- contracts/PPBToken.sol
+++ contracts/PPBToken.sol
@@ -6,6 +6,6 @@ pragma solidity ^0.6.0;
 
 contract PPBToken is ERC777 {
     constructor() public ERC777("PPBToken", "PPB", new address[](0)) {
-        _mint(msg.sender, 21e9 * 10e18, "", "");
+        _mint(msg.sender, 21e8 * 1e18, "", "");
     }
 }
